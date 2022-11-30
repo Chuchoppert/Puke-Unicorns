25923b12ed99c35d1838ea97eb74b4a6ef5bbff6
Chuchopper <chuchopper3d@gmail.com>
2022-11-30 13:47:38 -0600

    TMP agregado & mecánicas importantes agregadas
    Pastel malo
    - Recoger pastel malo, lanzar pastel con la misma tecla
    - Apuntado de 8 direcciones para lanzar
    - Efecto de pastel malo: "vomitar, regresar al peso normal e inmovilizar x tiempo, no se afectado de nuevo
    - icono que muestra dirección de lanzado cuando se tome el pastel y ocultar cuando no hay pastel por lanzar
    -Restar puntos al contricante envenenado y actualizar ui de puntos
    
    Pastel bueno
    - Misma tecla para agarrar pastel malo que para comer
    - comer en x tiempo
    - Sumar puntos por comer y actualizar puntos en el UI
    
    Player
    - Mostar envenenamiento
    - Mostrar barra de pastel por comer o tomar
    - Rotar player en 8 direcciones visuales

--- PukeNicorns_Unity/Assets/Script/CakeObject.cs
+++ PukeNicorns_Unity/Assets/Script/CakeObject.cs
@@ -4,59 +4,217 @@ public class CakeObject : MonoBehaviour
 {
     [SerializeField] bool isCakeSpoiled = false;
     [SerializeField] bool isGround = false;
+    [Space]
     [SerializeField] float TimeToSpoil = 5;
     [SerializeField] float TimeToDestroy = 5;
-    [SerializeField] float TimerCake;
+    [SerializeField] float TimeToEatCake = 5;
+    private float TimerCake;
 
+    private float TimerGrabCake;
+    private bool isCakeTaked = false;
+    private Collider playerCollider;
+
+    [Space]
+    [SerializeField] int PointsToAdd;
+    [SerializeField] int PointsToSubstrac;
+    [Space]
     [Header("Testing")]
     [SerializeField] Material materialGood;
 
+    //Solo para cambiar el estado del pastel
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.collider.CompareTag("Piso"))
+        //Evitar que se mueva o sea activado por otros objetos mientras cae.
+        if (collision.gameObject.CompareTag("Piso"))
         {
+            //Evitar movimiento extra al pastel
             this.GetComponent<Rigidbody>().useGravity = false;
+            this.GetComponent<Rigidbody>().isKinematic = true;
 
+            //Cambiar la forma de colision
             this.GetComponent<BoxCollider>().isTrigger = true;
             this.GetComponent<BoxCollider>().size = new Vector3(1.5f, 2.0f, 1.5f);
+
+            //Aqui cambiar de sprite "cayendo" a "desparramao", al igual que agregar sonido splash
+
+            //Una vez caido, activara el proceso GetSpoiledEffectOrDestroy()
             this.isGround = true;
         }
     }
 
-    private void OnTriggerEnter(Collider other)
+    //Solo deberia entrar aqui si el pastel ya cayo.
+    private void OnTriggerStay(Collider other)
+    {
+        //Solo si un personaje lo toca y este haya caido hacer: recoger pastel, comer pastel o afectar al personaje
+        if (other.CompareTag("Player") && this.isGround)
+        {
+            /////Solo casos en que el pastel no haya sido tomado.
+            //Si el player toca la tecla de accion de tomar y lanzar entonces activara el proceso GrabOrEatCake()
+            if (Input.GetKeyDown(other.GetComponent<MovPlayer>().GrabAndLaunch_Action) && isCakeTaked == false)
+            {
+                //Muestra el tiempo que tienes y que falta para tomar o comer el pastel
+                other.GetComponent<MovPlayer>().Slider_Eat.maxValue = TimeToEatCake;
+
+                //Guardar collider del player para los procesos de GrabOrEatCake()
+                playerCollider = other;
+            }
+            //Si el player levanta la tecla, se interrumpira el proceso
+            else if(Input.GetKeyUp(other.GetComponent<MovPlayer>().GrabAndLaunch_Action) && isCakeTaked == false)
+            {
+                //Quitar activador del proceso GrabOrEatCake() y reiniciar timer
+                playerCollider = null;
+                TimerGrabCake = 0.0f;
+            }
+            /////
+            
+            /////Este caso solo deberia pasar si es lanzado y toca al contrario
+            //Solo caso en que el pastel haya sido tomado y esta malo
+            else if (this.isCakeSpoiled && isCakeTaked)
+            {
+                //Si el nombre del pastel es el mismo del personaje que lo lanzo, no afectar.
+                if(other.name != name)
+                {
+                    //Afectar solo si el player no esta envenenado
+                    if(other.GetComponent<MovPlayer>().CakesEatsWithoutDigest != -1)
+                    {
+                        //Si es distinto entonces hacer el proceso EffectCake del script del player y restar puntos.
+                        other.GetComponent<MovPlayer>().EffectCake(isCakeSpoiled, 0, PointsToSubstrac);
+                    }
+
+                    //Despues de afectar, destruir
+                    Destroy(this.gameObject);
+                }
+            }
+        }
+
+    }
+
+    ///Solo para interrumpir el proceso de tomar o comer pastel, si el player ya salio del area del pastel
+    private void OnTriggerExit(Collider other)
     {
         if (other.CompareTag("Player") && this.isGround)
         {
-            other.GetComponent<MovPlayer>().EffectCake(isCakeSpoiled);
-            Destroy(this.gameObject);
+            //Quitar activador del proceso GrabOrEatCake() y reiniciar timer
+            TimerGrabCake = 0.0f;
+            playerCollider = null;
         }
     }
 
+    ///Revision de los procesos
     private void Update()
     {
+        GetSpoiledEffectOrDestroy();
+
+        if (playerCollider)
+        {
+            GrabOrEatCake(playerCollider);
+        }
+    }
+
+    //Metodo para pudrir pastel y destruir
+    void GetSpoiledEffectOrDestroy()
+    {
+        //Solo activar si el pastel toco el piso
         if (this.isGround)
         {
+            //Comenzar timer
             this.TimerCake += Time.deltaTime;
+
+            //Si el pastel esta bueno
             if (isCakeSpoiled == false)
             {
+                //Y el timer es igual o mayor al tiempo para pudrir pastel, hacer el pastel malo
                 if (this.TimerCake >= TimeToSpoil)
                 {
                     Debug.Log("Cake was spoil");
                     this.isCakeSpoiled = true;
-                    this.TimerCake = 0.0f;
 
+                    //Sonido player afectado por pastel malo
+
+                    //Reiniciar timer
+                    this.TimerCake = 0.0f;
+                    
+                    //Test, cambiar por sprite malo
                     this.GetComponent<MeshRenderer>().material = materialGood;
                 }
             }
+            //Pero si el pastel esta malo
             else
             {
-                if (this.TimerCake >= TimeToDestroy)
+                //Y ha pasado el tiempo (se agrega la otra condicion, para que no se elimine el pastel en las manos del player)
+                if (this.TimerCake >= TimeToDestroy && isCakeTaked == false)
                 {
                     Debug.Log("Cake wasted");
+
+                    //Si habra sonido de pastel destruido, agregar aqui
+
+                    //Destruir el pastel malo
                     Destroy(this.gameObject);
                 }
             }
 
+        }
+    }
+
+    //Metodo para tomar o comer pastel
+    void GrabOrEatCake(Collider other)
+    {
+        //Solo hacer el contador mientras el pastel no hay sido tomado
+        if (isCakeTaked == false)
+        {
+            this.TimerGrabCake += Time.deltaTime;
+            other.GetComponent<MovPlayer>().Slider_Eat.value = this.TimerGrabCake;
+
+            //Si hay sonido de player intentando tomar pastel, agregar aqui.
+        }
+        
+        //Si el contador es igual o mayor al tiempo para tomar pastel entones tomar o comer
+        if (this.TimerGrabCake >= TimeToEatCake)
+        {
+            //Tomar pastel solo si el player no tiene otro pastel y si el pastel es malo
+            if (isCakeSpoiled && other.GetComponent<MovPlayer>().havePlayerCake == false)
+            {
+                Debug.Log("Cake was taked");
+
+                //Sonido de pastel tomado
+
+
+                //Preparar el script del player para el lanzamiento
+                other.GetComponent<MovPlayer>().havePlayerCake = true;
+                other.GetComponent<MovPlayer>().cakeTaked_GO = this.gameObject;
+                other.GetComponent<MovPlayer>().UI_Direction.SetActive(true);
+
+                //Cambiar nombre a este pastel para evitar que afecte al personaje lanzador
+                name = other.name;
+
+                //quitar posibilidad de colisiones mientras este en la mano del player y que siga al player
+                GetComponent<BoxCollider>().enabled = false;
+                this.gameObject.transform.SetParent(other.gameObject.transform, true);
+
+                //Reset timer
+                this.TimerGrabCake = 0.0f;
+
+                isCakeTaked = true;
+            }
+            //De lo contrario solo comer pastel
+            else
+            {
+                Debug.Log("Cake was eated");
+
+                //Sonido pastel comido
+
+                //Hacer proceso EffectCake, sumando puntos
+                other.GetComponent<MovPlayer>().EffectCake(isCakeSpoiled, PointsToAdd);
+
+                //Reiniciar, por si acaso se repite
+                this.TimerGrabCake = 0.0f;
+
+                //Destruir
+                Destroy(this.gameObject);
+            }
+
+            //Reiniciar slider
+            other.GetComponent<MovPlayer>().Slider_Eat.value = 0;
         }
     }
 }
--- PukeNicorns_Unity/Assets/Script/MovPlayer.cs
+++ PukeNicorns_Unity/Assets/Script/MovPlayer.cs
@@ -1,28 +1,47 @@
-using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MovPlayer : MonoBehaviour
 {
     [Header("Vars for movement")]
     [Range(0.2f, 1f)] public float ComplexionBodySpeed = 1; //1 a 0.2; velocidad de peso
     [SerializeField] float SpeedMovement = 10; //Velocidad Base
+    private float InputY;
+    private float InputX;
 
     [Space, Header("Vars for effects from cake")]
-    public float TimeWhileEatCake = 3.0f; //tiempo en tardar en comer pastel
-    [SerializeField] float TimeToDigestCake = 3.0f; //tiempo para digerir pastel
-    [SerializeField] float TimeWhilePoison = 3.0f; //Tiempo inhabilitado cuando come pastel malo
-    [SerializeField, Range(0, 5)] int CakesEatsWithoutDigest = 0; //cantidad de pasteles sin terminar de digerir
+    [Range(0, 5)] public int CakesEatsWithoutDigest = 0; //cantidad de pasteles sin terminar de digerir
     [SerializeField] int RangeMaxFromFlatToFat = 5; //Cantidad maxima de pasteles para llegar a la gordura maxima
+    [SerializeField] float TimeToDigestCake = 3.0f; //tiempo en tardar en "digerir" el pastel
+    public Slider Slider_Eat;
+    private float timerPassed_Digest = 0;
+    [Space]
+    [SerializeField] float TimeWhilePoison = 3.0f; //Tiempo inhabilitado cuando come pastel malo
+    [SerializeField] RawImage Player_RawImage;
+    private float timerPassed_Poison = 0;
+
+    [Space, Header("Grab and Launchs vars")]
+    [SerializeField] float ForceToLauch = 5;
+    public KeyCode GrabAndLaunch_Action;
+    public bool havePlayerCake = false;
+    public GameObject cakeTaked_GO;
+    public GameObject UI_Direction;
+    [SerializeField] Vector3 DirToLaunch; 
+    [SerializeField] float RotationY;
+    private bool ReadyToLaunch = false;
+
+    [Space, Header("Dash vars")]
+    [SerializeField] KeyCode Dash_Action;
+
+    [Space, Header("Vars for points")]
+    [SerializeField] int PointsPlayer = 0;
+    [SerializeField] TextMeshProUGUI Points_TMP;
+    [SerializeField] RawImage DashIcon_RawImage;
 
     [Space, Header("Vars for invert movement")]
     [Range(-1, 1), SerializeField] float InvertMovementX = 1;
     [Range(-1, 1), SerializeField] float InvertMovementY = 1;
-
-    [Space, Header("Limits movement")]
-    [SerializeField] Vector2 HorizontalLimit;
-    [SerializeField] Vector2 VerticalLimit;
 
     [Space, Header("Extra Vars")]
     [SerializeField] Transform SpritePlayer;
@@ -30,77 +49,181 @@ public class MovPlayer : MonoBehaviour
     [SerializeField] Rigidbody rb;
 
     [Space, SerializeField] bool isPlayer1 = false;
-    [SerializeField] float timerPassedForCakeEffect = 0;
-    float InputY;
-    float InputX;
 
 
 
     void Update()
-    {
+    {        
+        //////////////////////////////////
         SpritePlayer.LookAt(new Vector3(0, -Camera.main.transform.position.x, Camera.main.transform.position.z));
 
+        ///Revisar que player es para recibir inputs
         if (isPlayer1)
         {
-            InputY = Input.GetAxis("Vertical") * InvertMovementX;
-            InputX = Input.GetAxis("Horizontal") * InvertMovementY;
+            InputY = Input.GetAxis("Vertical") * InvertMovementY;
+            InputX = Input.GetAxis("Horizontal") * InvertMovementX;
         }
         else
         {
-            InputY = Input.GetAxis("Vertical2") * InvertMovementX;
-            InputX = Input.GetAxis("Horizontal2") * InvertMovementY;
+            InputY = Input.GetAxis("Vertical2") * InvertMovementY;
+            InputX = Input.GetAxis("Horizontal2") * InvertMovementX;
         }
-
-        if(CakesEatsWithoutDigest > 0)
+        /////////////////////////////////////
+        
+        //Proceso para diferir pasteles 
+        if (CakesEatsWithoutDigest > 0)
         {
-            timerPassedForCakeEffect += Time.deltaTime;
-            if(timerPassedForCakeEffect >= TimeToDigestCake)
+            //empezar timer
+            timerPassed_Digest += Time.deltaTime;
+
+            //Si timer es igual o mayor que el tiempo para digerir eliminar un valor
+            if(timerPassed_Digest >= TimeToDigestCake)
             {
                 Debug.Log(this.gameObject.name + ": " + CakesEatsWithoutDigest + " to digest");
-                timerPassedForCakeEffect = 0;
+
+                //Resetear timer y restar pasteles sin digerir
+                timerPassed_Digest = 0;
                 CakesEatsWithoutDigest -= 1;
 
+                //Actualizar peso del player
                 EffectWeightMovement();
             }
         }
+        //Proceso player envenenado
+        else if(CakesEatsWithoutDigest == -1)
+        {
+            //empezar timer
+            timerPassed_Poison += Time.deltaTime;
+
+            //Si timer es igual o mayor que el tiempo de envenamiento quitar envenenamiento y hacer "flaco"
+            if (timerPassed_Poison >= TimeWhilePoison)
+            {
+                Debug.Log(this.gameObject.name + ": is now right!");
+
+                //resetear timer y pasteles sin digerir
+                timerPassed_Poison = 0;
+                CakesEatsWithoutDigest = 0;
+
+                //Test, cambiar por sprite normal
+                if (isPlayer1)
+                {
+                    Player_RawImage.color = Color.white;
+                }
+                else
+                {
+                    Player_RawImage.color = Color.yellow;
+                }
+
+                //Actulizar peso del player
+                EffectWeightMovement();
+            }
+        }
+        /////////////////////////////////////
+        //Proceso para conocer la rotacion que se debe tener
+        CheckDirection();
+
+        //Girar player 
+        this.gameObject.transform.rotation = Quaternion.Euler(0.0f, RotationY, 0.0f);
+
+        //Solo si el player tiene un pastel, preparar el lanzamiento y lanzar
+        if (havePlayerCake)
+        {
+            //Una vez que el player tenga el pastel, debera dejar de tocar la tecla para preparar el lanzamiento
+            if (Input.GetKeyUp(GrabAndLaunch_Action))
+            {
+                ReadyToLaunch = true;
+            }
+
+            //solo si el player vuelve a tomar la tecla y esta listo para larzar
+            if (Input.GetKey(GrabAndLaunch_Action) && ReadyToLaunch)
+            {
+                //Desparentar el objeto
+                cakeTaked_GO.transform.parent = null;
+
+                //Activar fisicas de unity, empujar hacia la direccion reciente
+                cakeTaked_GO.GetComponent<Rigidbody>().isKinematic = false;
+                cakeTaked_GO.GetComponent<Rigidbody>().AddForce(DirToLaunch * ForceToLauch, ForceMode.Impulse);
+
+                //Regresar el collider a la normalidad y activar colisiones
+                cakeTaked_GO.GetComponent<BoxCollider>().size = Vector3.one;
+                cakeTaked_GO.GetComponent<BoxCollider>().enabled = true;
+
+                //Ocultar direccion de lanzamiento
+                UI_Direction.SetActive(false);
+
+                //Resetear valores para el proximo pastel
+                cakeTaked_GO = null;
+                havePlayerCake = false;
+                ReadyToLaunch = false;
+            }
+        }
+        /////////////////////////////////////
     }
     void FixedUpdate()
     {
-        Vector3 movedirection = new Vector3(InputX, 0.0f, InputY);//.normalized;
-        //movedirection = transform.TransformDirection(movedirection);
+        //Siempre que el jugador no este envenenado, mover
+        if(CakesEatsWithoutDigest != -1)
+        {
+            Vector3 movedirection = new Vector3(InputX, 0.0f, InputY);//.normalized;
+                                                                      //movedirection = transform.TransformDirection(movedirection);
 
-        //rb.MovePosition(transform.position + movedirection * ((SpeedMovement * Time.deltaTime) * ComplexionBodySpeed));
-        ControllerPlayer.Move(movedirection * ((SpeedMovement * Time.deltaTime) * ComplexionBodySpeed));
+            //rb.MovePosition(transform.position + movedirection * ((SpeedMovement * Time.deltaTime) * ComplexionBodySpeed));
+            ControllerPlayer.Move(movedirection * ((SpeedMovement * Time.deltaTime) * ComplexionBodySpeed));
+        }      
     }
 
-    public void EffectCake(bool CakeWasGood)
+    //Metodo para afectar al player desde el script pastel
+    public void EffectCake(bool CakeWasGood, int AddPoints = 0, int SubstracPoints = 0)
     {
-        if (CakeWasGood == false) //Pastel bueno
+        //Si el pastel era bueno, actualizar pesos y puntuacion
+        if (CakeWasGood == false)
         {
-            timerPassedForCakeEffect = 0.0f;
+            //Resetear timer y sumar a los pasteles sin digerir
+            timerPassed_Digest = 0.0f;
             CakesEatsWithoutDigest += 1;
+
             Debug.Log(this.gameObject.name + ": Eat good cake");
+
+            //Actualizar peso del player
             EffectWeightMovement();
+
+            //Actualizar puntos
+            RefreshPointsAndUi(AddPoints);
         }
         else
         {
             Debug.Log(this.gameObject.name + ": Eat bad cake");
-            ////Vomitar pasteles, restar puntos, restar pasteles sin digerir, regresar variable de velocidad del peso
+
+            //Test, cambiar por sprite envenenado
+            Player_RawImage.color = Color.green;
+
+            // -1 por que vomita pasteles sin digerir y para empezar el proceso de envenamiento
+            CakesEatsWithoutDigest = -1;
+
+            //Actualizar puntos
+            RefreshPointsAndUi(SubstracPoints);
         }
     }
 
+    //Metodo para actualizar pesos (velocidad)
     void EffectWeightMovement()
     {
+        //Si no hay pasteles por digerir regresar la velocidad normal
         if(CakesEatsWithoutDigest == 0)
         {
             Debug.Log(this.gameObject.name + ": All cakes digested");
+
             ComplexionBodySpeed = 1.0f;
         }
+
+        ///Si los pasteles por digerir son igual o menor que el rango max para hacer pesado al player, cambiar velocidad
         else if (CakesEatsWithoutDigest <= RangeMaxFromFlatToFat)
         {          
+            //el rango de velocidad / el numero max para hacer pesado al player = proporcion de velocidad por cada unidad de pastel comida
             float ValueForSpeed = (1.0f - 0.2f) / RangeMaxFromFlatToFat;
-            ComplexionBodySpeed = 0.2f + (ValueForSpeed * (RangeMaxFromFlatToFat - CakesEatsWithoutDigest));
 
+            //Velocidad = rango minimo + (la proporcion de velocidad * (porciones de velocidad))
+            ComplexionBodySpeed = 0.2f + (ValueForSpeed * (RangeMaxFromFlatToFat - CakesEatsWithoutDigest));
             //Ejemplo con rango maximo de 5, valueForSpeed = (0.8f)/5 = 0.16f
             //case 0 (5): 0.2 + (x * (5 - 0)); = 1.0f segun el rango maximo sera aprox 1.0f mejor se deja en un if
             //case 1 (4): 0.2 + (x * (5 - 1)); = 0.84f
@@ -108,6 +231,91 @@ public class MovPlayer : MonoBehaviour
             //case 3 (2): 0.2 + (x * (5 - 3)); = 0.52f
             //case 4 (1): 0.2 + (x * (5 - 4)); = 0.36f
             //case 5 (1): 0.2 + (x * (5 - 5)); = 0.2f
+
+            //segun el numero de sprites, agregar aqui el cambio de sprites de flaco a gordo y si hay sonido de gordura caminando agregar
+
+            //Limitar pasteles a "digerir", dejando que solo tenga que reducir este limite de pasteles.
+            if (CakesEatsWithoutDigest == RangeMaxFromFlatToFat) CakesEatsWithoutDigest = RangeMaxFromFlatToFat;
         }
+    }
+
+    //Metodo para actualizar el ui y el puntaje
+    void RefreshPointsAndUi(int ValueEffect)
+    {
+        //suma o resta puntaje
+        PointsPlayer += ValueEffect;
+
+        //limitar el minimo de puntos a 0
+        if(PointsPlayer < 0)
+        {
+            PointsPlayer = 0;
+        }
+
+        //actualizar texto de puntos.
+        Points_TMP.text = $"Puntos: {PointsPlayer}";
+    }
+
+    //Metodo para checar direction
+    void CheckDirection()
+    {
+        //x: izquierda|derecha
+        //y: arriba|abajo
+
+        //Agregar los sprites o giros del sprite
+        if (InputX == 0 && InputY == 0) //Dejar para evitar un bug de siempre mirar a 45° 
+        {
+            //RotationY = 0.0f;
+        }
+        else if (InputX == 0 && Mathf.Sign(InputY) != 0) //Casos para movimiento solo vertical
+        {
+            if (InputX == 0 && Mathf.Sign(InputY) == 1) //abajo
+            {
+                RotationY = 0.0f;
+                DirToLaunch = new Vector3(0.0f, 0.0f, 1.0f);
+            } 
+            else if (InputX == 0 && Mathf.Sign(InputY) == -1) //arriba
+            {
+                RotationY = 180.0f;
+                DirToLaunch = new Vector3(0.0f, 0.0f, -1.0f);
+            }         
+        }
+        else if (Mathf.Sign(InputX) != 0 && InputY == 0)//Casos para movimiento solo horizontal
+        {
+            if (Mathf.Sign(InputX) == 1 && InputY == 0) //derecha
+            {
+                RotationY = 90.0f;
+                DirToLaunch = new Vector3(1.0f, 0.0f, 0.0f);
+            }
+            else if (Mathf.Sign(InputX) == -1 && InputY == 0) //izquierda
+            {
+                RotationY = -90.0f;
+                DirToLaunch = new Vector3(-1.0f, 0.0f, 0.0f);
+            }
+        }
+        else                                            //Casos para diagonales
+        {
+            if (Mathf.Sign(InputX) == 1 && Mathf.Sign(InputY) == 1) //arriba-derecha
+            {
+                RotationY = 45.0f;
+                DirToLaunch = new Vector3(1.0f, 0.0f, 1.0f);
+            }
+            else if (Mathf.Sign(InputX) == -1 && Mathf.Sign(InputY) == 1) //arriba-izquierda
+            {
+                RotationY = -45.0f;
+                DirToLaunch = new Vector3(-1.0f, 0.0f, 1.0f);
+            }
+
+            else if (Mathf.Sign(InputX) == 1 && Mathf.Sign(InputY) == -1) //abajo-derecha
+            {
+                RotationY = 135.0f;
+                DirToLaunch = new Vector3(1.0f, 0.0f, -1.0f);
+            }
+            else if (Mathf.Sign(InputX) == -1 && Mathf.Sign(InputY) == -1) //abajo-izquierda
+            {
+                RotationY = -135.0f;
+                DirToLaunch = new Vector3(-1.0f, 0.0f, -1.0f);
+            }
+        }
+            
     }
 }
